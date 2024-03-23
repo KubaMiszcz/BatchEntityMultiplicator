95f8e683e53756e5c795aa8214b8f351bf4a795e
KubaMiszcz <zielonyeufor@gmail.com>
2024-03-23 14:04:54 +0100

    before switch to allkeys as strng

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { KeyValues } from './../../models/KeyValues.model';
 import { IKeyValues } from '../../models/KeyValues.model';
 import { combineLatestWith } from 'rxjs';
 import { AppService } from '../../services/app.service';
@@ -9,11 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-
   result = 'result';
   template = EXAMPLE_TEMPLATE;
 
-  keyValues: IKeyValues[] = EXAMPLE_KEYVALUES;
+  allKeyValues: IKeyValues[] = EXAMPLE_KEYVALUES;
 
   string1key = '{cn}';
   strings1values = 'Maniek\nGrazyna';
@@ -30,6 +30,65 @@ export class HomePageComponent implements OnInit {
   generate() {
     // check if same lengyth in stringvalues
     let result = '';
+    let item = this.template;
+
+    let count = this.allKeyValues[0]?.values.length;
+    for (let index = 0; index < count; index++) {
+      let item = this.template;
+      let keyValues;
+
+      item = item.replaceAll(this.string1key, strings1valuesArray[index]);
+      item = item.replaceAll(this.string2key, strings2valuesArray[index]);
+
+      result += item;
+      result += '\n\n';
+    }
+
+
+
+  }
+  generate2() {
+    // check if same lengyth in stringvalues
+    let result = '';
+    let item = this.template;
+
+    let count = this.allKeyValues[0]?.values.length;
+    for (let index = 0; index < count; index++) {
+      let item = this.template;
+      let keyValues;
+
+      item = item.replaceAll(this.string1key, strings1valuesArray[index]);
+      item = item.replaceAll(this.string2key, strings2valuesArray[index]);
+
+      result += item;
+      result += '\n\n';
+    }
+
+
+    this.allKeyValues.forEach((keyValues) => {
+      let count = keyValues.values.length;
+
+      for (let index = 0; index < count; index++) {
+        item = item.replaceAll(keyValues.key, keyValues.values[index]);
+
+        result += item;
+        result += '\n\n';
+      }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     let strings1valuesArray = this.strings1values.split('\n');
     let strings2valuesArray = this.strings2values.split('\n');
 
@@ -37,6 +96,8 @@ export class HomePageComponent implements OnInit {
 
     for (let index = 0; index < count; index++) {
       let item = this.template;
+      let keyValues;
+
       item = item.replaceAll(this.string1key, strings1valuesArray[index]);
       item = item.replaceAll(this.string2key, strings2valuesArray[index]);
 
@@ -48,12 +109,12 @@ export class HomePageComponent implements OnInit {
   }
 
   addKey() {
-    throw new Error('Method not implemented.');
+    this.allKeyValues.push(new KeyValues());
   }
 }
 
 export const EXAMPLE_TEMPLATE =
-'# Entry 1: cn={cn},ou=users,ou=312klp-SZKOL,dc=ldap,dc=local\ndn: cn={cn},ou=users,ou=312klp-SZKOL,dc=ldap,dc=local\ncn: {cn}\ngidnumber: 2001\nhomedirectory: /home/users/{cn}\nobjectclass: inetOrgPerson\nobjectclass: posixAccount\nobjectclass: top\nsn: {cn}\nuid: {cn}\nuidnumber: {uidNo}';
+  '# Entry 1: cn={cn},ou=users,ou=312klp-SZKOL,dc=ldap,dc=local\ndn: cn={cn},ou=users,ou=312klp-SZKOL,dc=ldap,dc=local\ncn: {cn}\ngidnumber: 2001\nhomedirectory: /home/users/{cn}\nobjectclass: inetOrgPerson\nobjectclass: posixAccount\nobjectclass: top\nsn: {cn}\nuid: {cn}\nuidnumber: {uidNo}';
 
 export const EXAMPLE_KEYVALUES = [
   {
